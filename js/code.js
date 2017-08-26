181523bb41d2be78f6452221b232375494477639
monialisa91 <monikarichter18@gmail.com>
2017-08-27 01:25:07 +0200

    the conditional statement code

---
+++ js/code.js
@@ -0,0 +1,13 @@
+var a = prompt("Give the first value");
+var b = prompt("Give the second value");
+
+var value = a * a + 2 * a * b - b * b;
+console.log("The result of this operation is " + value);
+
+if(value > 0){
+	console.log("The value is positive");
+}else if(value < 0){
+	console.log("The value is negative");
+}else{
+	console.log("The value is equal to 0");
+}
