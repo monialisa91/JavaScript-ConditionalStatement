c66e2c3b6db49a9cd48b0f4300532852ffac1ced
monialisa91 <monikarichter18@gmail.com>
2017-08-28 13:15:08 +0200

    spaces added

--- js/code.js
+++ js/code.js
@@ -4,10 +4,12 @@ var b = prompt("Give the second value");
 var value = a * a + 2 * a * b - b * b;
 console.log("The result of this operation is " + value);
 
-if(value > 0){
+if(value > 0) {
 	console.log("The value is positive");
-}else if(value < 0){
+}
+else if(value < 0) {
 	console.log("The value is negative");
-}else{
+}
+else {
 	console.log("The value is equal to 0");
 }
